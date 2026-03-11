74d8307b086b1f58a8d9789ae96c6b13049d33ca
coriander-curry <corriejcurry@gmail.com>
2026-03-10 23:53:44 -0700

    about fix

--- public/layout.js
+++ public/layout.js
@@ -86,7 +86,7 @@ const headerEl = `
 	        <nav>
 	          <ul>
               <li><a href="index.html">Home</a></li>
-	            <li><a href="about-me.html">About</a></li>
+	            <li><a href="about.html">About</a></li>
 	            <li><a href="tutorials.html">Tutorials</a></li>
               <li><a href="projects.html">Projects</a></li>
 	            <li><a href="blog.html">Blog</a></li>
